b8cf1b134b25d2ea0e565c166eb081afc05291f9
Andres Velasco <velasco.andrs@gmail.com>
2024-07-22 14:18:16 -0500

    feat(english-day): setup reminder endpoint

--- src/router/reminders.rs
+++ src/router/reminders.rs
@@ -22,7 +22,7 @@ async fn reminder_good_night(State(ctx): State<RouterState>) -> impl IntoRespons
     if let Err(err) = response {
         error!("Error on send message: {err}");
 
-        return (StatusCode::BAD_GATEWAY, "Error on publish repositories").into_response();
+        return (StatusCode::BAD_GATEWAY, "Error on  \"good night\" reminder").into_response();
     }
 
     info!("Message was sending to channel [GENERAL]");
@@ -42,7 +42,30 @@ async fn reminder_good_morning(State(ctx): State<RouterState>) -> impl IntoRespo
     if let Err(err) = response {
         error!("Error on send message: {err}");
 
-        return (StatusCode::BAD_GATEWAY, "Error on publish repositories").into_response();
+        return (
+            StatusCode::BAD_GATEWAY,
+            "Error on send  \"good morning\" reminder",
+        )
+            .into_response();
+    }
+
+    info!("Message was sending to channel [GENERAL]");
+
+    (StatusCode::OK, "Published \"good morning\" reminder").into_response()
+}
+
+async fn reminder_english_day(State(ctx): State<RouterState>) -> impl IntoResponse {
+    let response = utils::send_message_to_channel(
+        &ctx.0,
+        consts::GENERAL_CHANNEL_ID,
+        "@here\n Today is the english day".to_string(),
+    )
+    .await;
+
+    if let Err(err) = response {
+        error!("Error on send message: {err}");
+
+        return (StatusCode::BAD_GATEWAY, "Error ").into_response();
     }
 
     info!("Message was sending to channel [GENERAL]");
@@ -54,4 +77,5 @@ pub fn build_router() -> Router<RouterState> {
     Router::new()
         .route("/good-night", post(reminder_good_night))
         .route("/good-morning", post(reminder_good_morning))
+        .route("/english-day", post(reminder_english_day))
 }
