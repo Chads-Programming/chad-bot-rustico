8ea8768bfa6aebd31a3ddf70ce494b7f996c1e9f
Andres Velasco <velasco.andrs@gmail.com>
2025-04-29 23:57:40 -0500

    fix: solve fmt

--- src/book.rs
+++ src/book.rs
@@ -5,7 +5,10 @@ use crate::{errors::CustomError, router::book_club::BookSubmission};
 pub async fn create_book_embeb(book: BookSubmission) -> Result<CreateEmbed, CustomError> {
     let embed = CreateEmbed::new()
         .title(book.title)
-        .description(format!("{}\n\nvotes: `{}`\npowered by: <@{}>",book.description,book.votes, book.creator_id))
+        .description(format!(
+            "{}\n\nvotes: `{}`\npowered by: <@{}>",
+            book.description, book.votes, book.creator_id
+        ))
         .footer(CreateEmbedFooter::new(book.authors.join("-").to_string()))
         .image(book.cover_url);
 
